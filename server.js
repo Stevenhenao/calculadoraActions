f6f46ae1914e5f79907aa650bfe2a9a25bb88672
steven <steven.henao@udea.edu.co>
2022-09-09 19:28:25 -0500

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const api = require("./api.calculator");
 
-const port = app.listen(process.env.PORT || 3000);
+const port = api.listen(process.env.PORT || 3000);
 
 api.listen(port, () => {
     console.log("running")
